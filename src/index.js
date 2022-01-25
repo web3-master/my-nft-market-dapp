9385f0b79430ffdf4230f4a5e9b3325fe336baea
Daniel Armstrong <dany.armstrong90@gmail.com>
2022-01-25 21:32:14 +0800

    worked on web3 integration
    -metamask connect.
    -web3 related react context code.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Web3Provider from "./web3/store/Web3Provider";
+import CollectionProvider from "./web3/store/CollectionProvider";
+import MarketplaceProvider from "./web3/store/MarketplaceProvider";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3Provider>
+      <CollectionProvider>
+        <MarketplaceProvider>
+          <App />
+        </MarketplaceProvider>
+      </CollectionProvider>
+    </Web3Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/menu/AppMenu.js
+++ src/menu/AppMenu.js
@@ -1,10 +1,23 @@
 import { Menu } from "antd";
-import { AppstoreOutlined, PlusOutlined } from "@ant-design/icons";
+import {
+  AppstoreOutlined,
+  PlusOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import { Link } from "react-router-dom";
 
+import React, { useContext, useState } from "react";
+import web3 from "../web3/connection/web3";
+import Web3Context from "../web3/store/web3-context";
+import CollectionContext from "../web3/store/collection-context";
+import MarketplaceContext from "../web3/store/marketplace-context";
+
 const AppMenu = () => {
+  const web3Ctx = useContext(Web3Context);
+  const isConnected = web3 && web3Ctx.account;
+
   return (
-    <Menu mode="horizontal" style={{ backgroundColor: "transparent" }}>
+    <Menu mode="horizontal">
       <Menu.Item key="market" icon={<AppstoreOutlined />}>
         Market
         <Link to="/market" />
